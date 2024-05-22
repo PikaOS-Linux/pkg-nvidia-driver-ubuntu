047ab3c51b7ba736c2e6866475a67c85df637227
Renaud Lepage <root@cybikbase.com>
2024-05-22 09:35:02 -0700

    555.42.02-0pikaprep1 (patches unapplied)

---
+++ makedriver.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd "${SCRIPT_DIR}"
+
+DRIVER_VERSION_MAJOR="555"
+DRIVER_VERSION_FULL="555.42.02"
+DRIVER_ARCH="Linux-x86_64"
+DEBIAN_ARCH="$1"
+DEBIAN_FRONTEND=noninteractive
+
+mkdir -p "amd64"
+
+pushd "amd64" || exit 1
+wget -c \
+    https://us.download.nvidia.com/XFree86/$DRIVER_ARCH/$DRIVER_VERSION_FULL/NVIDIA-$DRIVER_ARCH-$DRIVER_VERSION_FULL.run
+chmod +x *.run
+
+popd || exit 2
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage --no-sign -b -a "${DEBIAN_ARCH}"
+
+popd

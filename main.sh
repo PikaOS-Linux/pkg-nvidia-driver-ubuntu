e70b91a87dade34117a98be3f58b176935f4e3a7
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-06-16 20:02:11 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,5 +5,8 @@ cd ./ubuntu-nvidia-drivers-mirror
 chmod +x ./makedriver.sh && ./makedriver.sh "$1"
 
 # Move the debs to output
+sudo apt install -y tree
+tree
+
 mkdir -p ./output
 mv ./*.deb ./output/

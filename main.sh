29dd77c0b896150f3d8d1163ddb71017c8a5258f
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-06-16 20:00:29 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 cd ./ubuntu-nvidia-drivers-mirror
 
-chmod +x ./makedriver.sh && /makedriver.sh "$1"
+chmod +x ./makedriver.sh && ./makedriver.sh "$1"
 
 # Move the debs to output
 mkdir -p ./output
